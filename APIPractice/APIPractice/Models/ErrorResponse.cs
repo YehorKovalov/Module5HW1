f0ecee3842e06e1e2575436542b06a599d5e48e7
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-02-06 22:57:37 +0200

    Class with Http methods is added

---
+++ APIPractice/APIPractice/Models/ErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace APIPractice.Models
+{
+    public class ErrorResponse
+    {
+        [JsonProperty("error")]
+        public string ErrorMessage { get; set; }
+    }
+}
