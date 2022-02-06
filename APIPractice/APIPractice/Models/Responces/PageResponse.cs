a06bc1fd05b0eaaf2b0377849b0c489b47da50e2
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-02-06 22:58:57 +0200

    Responces are added

---
+++ APIPractice/APIPractice/Models/Responces/PageResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace APIPractice.Models.Responces
+{
+    public class PageResponse<T>
+    {
+        [JsonProperty("page")]
+        public int Page { get; set; }
+
+        [JsonProperty("per_page")]
+        public int PerPage { get; set; }
+
+        [JsonProperty("total")]
+        public int Total { get; set; }
+
+        [JsonProperty("total_pages")]
+        public int TotalPages { get; set; }
+
+        [JsonPropertyName("data")]
+        public T Data { get; set; }
+    }
+}
---
+++ APIPractice/APIPractice/Models/Responces/UserDetailDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace APIPractice.Models.Responces
+{
+    public class UserDetailDTO
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("job")]
+        public string Job { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTimeOffset CreationTime { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public DateTimeOffset UpdatingTime { get; set; }
+    }
+}
