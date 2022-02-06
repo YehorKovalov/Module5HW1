977645d947ad5edf00b8c1700bfcb9f4edaee95e
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-02-06 22:58:41 +0200

    Models are added

---
+++ APIPractice/APIPractice/Models/Support.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace APIPractice.Models
+{
+    public class Support
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
---
+++ APIPractice/APIPractice/Models/UserAccount.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace APIPractice.Models
+{
+    public class UserAccount
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+}
