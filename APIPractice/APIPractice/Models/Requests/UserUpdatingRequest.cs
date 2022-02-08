14a21b2449f727897db7e90139be0bae004d2105
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-02-08 02:27:42 +0200

    Requests folder with models is added, Helpers folder is deleted

---
+++ APIPractice/APIPractice/Models/Requests/UserUpdatingRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace APIPractice.Models.Requests
+{
+    public class UserUpdatingRequest
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("job")]
+        public string Job { get; set; }
+    }
+}
